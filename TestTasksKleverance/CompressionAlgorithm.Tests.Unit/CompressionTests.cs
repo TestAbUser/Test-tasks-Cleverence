5f9c5c9019dc7ba3a1b8d849a169e56cea4d4f36
Rashid Isaev <isarutul@yandex.ru>
2025-04-03 21:05:21 +0300

    Implement case when line is empty or null

---
+++ TestTasksKleverance/CompressionAlgorithm.Tests.Unit/CompressionTests.cs
@@ -0,0 +1,16 @@
+﻿namespace CompressionAlgorithm.Tests.Unit
+{
+    public class CompressionTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Line_is_empty_or_null(string? line)
+        {
+            Assert.ThrowsAny<ArgumentException>(() =>
+            Compressor.Compress(line));
+        }
+
+
+    }
+}
---
+++ TestTasksKleverance/CompressionAlgorithm/Compressor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressionAlgorithm
+{
+    public static class Compressor
+    {
+        public static string Compress(string? line)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(line, nameof(line));
+            string compressedString = line;
+            return compressedString;
+        }
+    }
+}
