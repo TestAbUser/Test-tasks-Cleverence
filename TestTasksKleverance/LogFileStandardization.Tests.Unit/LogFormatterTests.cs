28a81e5bf8c10ba6b78e3003f5a827914e01a46b
Rashid Isaev <isarutul@yandex.ru>
2025-04-09 23:26:38 +0300

    Implement date formatting

---
+++ TestTasksKleverance/LogFileStandardization.Tests.Unit/LogFormatterTests.cs
@@ -0,0 +1,20 @@
+﻿namespace LogFileStandardization.Tests.Unit
+{
+    public class LogFormatterTests
+    {
+        [Fact]
+        public void Transform_log_entries_from_format1_to_a_new_format()
+        {
+            //arrange
+            string format1 = "10.03.2025";
+            var sut = new LogFormatter();
+
+            // act
+            string result =sut.Format(format1);
+
+            //assert
+            var newFormat = "2025-03-10";
+            Assert.Equal(newFormat, result);
+        }
+    }
+}
---
+++ TestTasksKleverance/LogFileStandardization/LogFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LogFileStandardization
+{
+    public class LogFormatter
+    {
+
+        public string Format(string log)
+        {
+            // Regex regex = new();
+            string newLog = "";//new string[log.Length];
+            //foreach (var item in log)
+            //{
+            var culture= CultureInfo.GetCultureInfo("ru-RU");
+                if(DateOnly.TryParse(log, culture, out DateOnly date))
+                {
+                    newLog = log.Replace(log, date.ToString("o", CultureInfo.InvariantCulture));
+                }
+           // }
+            return newLog;
+        }
+    }
+}
