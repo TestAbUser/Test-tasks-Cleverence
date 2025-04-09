70acc281194a30655e26d5811b2cd86c89f34951
Rashid Isaev <isarutul@yandex.ru>
2025-04-09 19:15:37 +0300

    Add initial implementation of Server class

--- TestTasksKleverance/StaticServer/Program.cs
+++ TestTasksKleverance/StaticServer/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StaticServer;
+var IncrementValue = 0;
+Parallel.For(0, 1000, _ =>
+{
+   // IncrementValue+=1;
+    Server.AddToCount(1);
+   // Server.AddToCount(2);
+    IncrementValue = Server.GetCount();
+});
+
+Console.WriteLine("Expected Result: 10000");
+Console.WriteLine($"Actual Result: {Server.GetCount()}");
+Console.ReadLine();
--- TestTasksKleverance/StaticServer/Server.cs
+++ TestTasksKleverance/StaticServer/Server.cs
@@ -9,20 +9,20 @@ namespace StaticServer
     public static class Server
     {
         private static int count;
-        private static readonly object _locker = new object();
+        private static readonly object _locker = new();
         public static int GetCount()
         {
             return count;
         }
 
-        public static int AddToCount(int value)
+        public static void AddToCount(int value)
         {
             lock (_locker)
             {
-                count = value;
+                count += value;
             }
+            //count+=value;
 
-            return count;
         }
     }
 }
